32d38970da70442d58b5120e1485ffb79109111b
fujunnnn <85174995+fujunnnn@users.noreply.github.com>
2025-08-03 12:12:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const svg = d3.select("#vis");
 const width = +svg.attr("width");
 const height = +svg.attr("height");
@@ -16,37 +15,42 @@ d3.csv("dataset.csv").then(data => {
 function setScene(sceneIndex) {
   state.scene = sceneIndex;
   svg.selectAll("*").remove(); // Clear previous scene
+  
+  if (sceneIndex === 0) scene1();
+  else if (sceneIndex === 1) scene2();
+  else if (sceneIndex === 2) scene3();
+}
 
-  clearScene();
-
+// Scene 1: Basic scatterplot
+function scene1() {
   const margin = { top: 40, right: 150, bottom: 50, left: 60 };
   const plotWidth = width - margin.left - margin.right;
   const plotHeight = height - margin.top - margin.bottom;
 
+  // Define x and y scales
   const x = d3.scaleLinear()
-    .domain([d3.min(data, d => d.horsepower) - 5, d3.max(data, d => d.horsepower) + 5])
+    .domain([d3.min(state.data, d => +d.horsepower) - 10, d3.max(state.data, d => +d.horsepower) + 10])
     .range([margin.left, margin.left + plotWidth]);
 
   const y = d3.scaleLinear()
-    .domain([d3.min(data, d => d.mpg) - 2, d3.max(data, d => d.mpg) + 2])
+    .domain([d3.min(state.data, d => +d.mpg) - 2, d3.max(state.data, d => +d.mpg) + 2])
     .range([margin.top + plotHeight, margin.top]);
 
-  const xAxis = d3.axisBottom(x);
-  const yAxis = d3.axisLeft(y);
-
+  // Add x-axis
   svg.append("g")
     .attr("transform", `translate(0,${margin.top + plotHeight})`)
-    .call(xAxis)
+    .call(d3.axisBottom(x))
     .append("text")
     .attr("x", plotWidth / 2)
-    .attr("y", 35)
+    .attr("y", 40)
     .attr("fill", "black")
     .style("text-anchor", "middle")
     .text("Horsepower");
 
+  // Add y-axis
   svg.append("g")
     .attr("transform", `translate(${margin.left},0)`)
-    .call(yAxis)
+    .call(d3.axisLeft(y))
     .append("text")
     .attr("transform", "rotate(-90)")
     .attr("x", -plotHeight / 2)
@@ -54,41 +58,14 @@ function setScene(sceneIndex) {
     .attr("fill", "black")
     .style("text-anchor", "middle")
     .text("MPG");
-
-  // Add legend
-  const legend = svg.selectAll(".legend")
-    .data(Object.entries(originColor))
-    .enter()
-    .append("g")
-    .attr("class", "legend")
-    .attr("transform", (d, i) => `translate(${width - 100},${50 + i * 25})`);
-
-  legend.append("rect")
-    .attr("x", 0)
-    .attr("width", 12)
-    .attr("height", 12)
-    .style("fill", d => d[1]);
-
-  legend.append("text")
-    .attr("x", 20)
-    .attr("y", 10)
-    .text(d => d[0]);
-
-
-  if (sceneIndex === 0) scene1();
-  else if (sceneIndex === 1) scene2();
-  else if (sceneIndex === 2) scene3();
-}
-
-
-////////////////////////////////////////////////////////
-function scene1() {
+	
   svg.append("text")
     .attr("x", 20)
     .attr("y", 30)
     .text("Scene 1: Overview")
     .style("font-size", "18px");
 
+  // Draw scatterplot circles
   svg.selectAll("circle")
     .data(state.data)
     .enter()
@@ -98,10 +75,11 @@ function scene1() {
     .attr("r", 5)
     .attr("fill", "steelblue");
 
+  // Annotations
   addAnnotation("High efficiency", 150, 200);
 }
 
-////////////////////////////////////////////////////////////
+// Scene 2: Filtered or sorted data
 function scene2() {
   svg.append("text")
     .attr("x", 20)
@@ -121,7 +99,7 @@ function scene2() {
   addAnnotation("Cars >30 MPG", 120, 180);
 }
 
-///////////////////////////////////////////////////////////////
+// Scene 3: Categorical color encoding
 function scene3() {
   svg.append("text")
     .attr("x", 20)
@@ -143,9 +121,9 @@ function scene3() {
     .attr("fill", d => colorScale(d.origin));
 
   addAnnotation("Country origin colored", 160, 220);
-  addLegend(colorScale);
 }
 
+// Annotation helper
 function addAnnotation(label, x, y) {
   const annotations = [{
     note: {
@@ -163,28 +141,3 @@ function addAnnotation(label, x, y) {
 
   svg.append("g").call(makeAnnotations);
 }
-
-function addLegend(colorScale) {
-  const legend = svg.append("g")
-    .attr("transform", `translate(${width - 150}, 60)`);
-
-  const origins = colorScale.domain();
-  origins.forEach((origin, i) => {
-    legend.append("rect")
-      .attr("x", 0)
-      .attr("y", i * 20)
-      .attr("width", 12)
-      .attr("height", 12)
-      .attr("fill", colorScale(origin));
-
-    legend.append("text")
-      .attr("x", 20)
-      .attr("y", i * 20 + 10)
-      .text(origin)
-      .style("font-size", "12px")
-      .attr("alignment-baseline", "middle");
-  });
-}
-
-
-
